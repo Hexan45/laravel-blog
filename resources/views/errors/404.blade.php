17bb7a1d143818030d52e933999abb474928a10e
Hexan45 <matwinski2@gmail.com>
2022-12-09 13:43:17 +0100

    Fixed 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -66,28 +66,7 @@
                 @unless(Auth::check())
                     <a href="{{ route('user.login') }}" class="primary submit">Zaloguj się</a>
                 @else
-                    <div class="content" onclick="toggleDrop(this)">
-                        <span class="user_nickname">
-                            <span style="font-weight: 500;">Witaj,</span> 
-                            {{ Auth::user()->nickname }}
-                        </span>
-                        <img src="{{ asset('storage/' . Auth::user()->image_path) }}" alt="Logged user avatar" style="width: 35px; height: 35px;" class="user_image show_drop" />
-
-                        <div class="dropdown">
-                            <ul>
-                                <li class="dropdown-account">Konto {{ Auth::user()->role }}a</li>
-                                <a href="#">
-                                    <li class="dropdown-item">Pokaż profil</li>
-                                </a>
-                                <a href="#">
-                                    <li class="dropdown-item">Ustawienia</li>
-                                </a>
-                                <a href="{{ route('user.logout') }}">
-                                    <li class="lined dropdown-item">Wyloguj się</li>
-                                </a>
-                            </ul>
-                        </div>
-                    </div>
+                    <x-user-settings />
                 @endunless
 
             </div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DefaultController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,55 +19,31 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::controller(DefaultController::class)->group(function () {
-    Route::get('/', 'index')
-        ->name('default.home');
+    Route::get('/', 'index')->name('default.home');
 
-    Route::get('/articles', 'articles')
-        ->name('default.articles')
-        ->middleware('auth.user:loggedIn');
+    Route::get('/articles', 'articles')->name('default.articles')->middleware('auth.user:loggedIn');
 
-    Route::get('/article/{article}', 'article')
-        ->where('id', '[0-9]+')
-        ->name('default.article')
-        ->middleware('auth.user:loggedIn');
+    Route::get('/article/{article}', 'article')->where('id', '[0-9]+')->name('default.article')->middleware('auth.user:loggedIn');
 
-    Route::get('/about-me', 'about')
-        ->name('default.about')
-        ->middleware('auth.user:loggedIn');
+    Route::get('/about-me', 'about')->name('default.about')->middleware('auth.user:loggedIn');
 
-    Route::get('/contact', 'contact')
-        ->name('default.contact')
-        ->middleware('auth.user:loggedIn');
+    Route::get('/contact', 'contact')->name('default.contact')->middleware('auth.user:loggedIn');
 });
 
 Route::prefix('user')->group(function () {
-    Route::get('/login', [LoginController::class, 'login'])
-        ->name('user.login')
-        ->middleware('auth.user:notLoggedIn');
+    Route::get('/login', [LoginController::class, 'login'])->name('user.login')->middleware('auth.user:notLoggedIn');
 
-    Route::post('/login', [LoginController::class, 'loginAuth'])
-        ->name('user.login.auth')
-        ->middleware('auth.user:notLoggedIn');
+    Route::post('/login', [LoginController::class, 'loginAuth'])->name('user.login.auth')->middleware('auth.user:notLoggedIn');
 
-    Route::get('/register', [RegisterController::class, 'register'])
-        ->name('user.register')
-        ->middleware('auth.user:notLoggedIn');
+    Route::get('/register', [RegisterController::class, 'register'])->name('user.register')->middleware('auth.user:notLoggedIn');
 
-    Route::post('/register', [RegisterController::class, 'registerValidate'])
-        ->name('user.register.validate')
-        ->middleware('auth.user:notLoggedIn');
+    Route::post('/register', [RegisterController::class, 'registerValidate'])->name('user.register.validate')->middleware('auth.user:notLoggedIn');
 
-    Route::get('/auth-token/{token}', [AuthController::class, 'emailVerify'])
-        ->name('user.auth.token')
-        ->where('token', '[a-zA-Z0-9]+')
-        ->middleware('auth.user:notLoggedIn');
+    Route::get('/auth-token/{token}', [AuthController::class, 'emailVerify'])->name('user.auth.token')->where('token', '[a-zA-Z0-9]+')->middleware('auth.user:notLoggedIn');
     
-    Route::get('/logout', [AuthController::class, 'logout'])
-        ->name('user.logout')
-        ->middleware('auth.user:loggedIn');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('user.logout')->middleware('auth.user:loggedIn');
 });
 
-    //404 page
-Route::fallback(function() {
-    return view('_404');
+Route::middleware(['auth.user:loggedIn', 'auth.user.role:Administrator'])->prefix('admin')->group(function() {
+    Route::get('dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
 });
